9f4b48814c8ca6d446e7afd765f189544880f1f0
zoechi <gzoechi@gmail.com>
2015-11-12 18:14:25 +0100

    - improve google-apis-demo (using binding instead of `.innerHtml`)

--- web/google_apis/google_apis_demo.dart
+++ web/google_apis/google_apis_demo.dart
@@ -11,32 +11,47 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 library polymer_elements_demos.web.google_apis.google_apis_demo;
 
 import 'dart:html' as dom;
-import 'dart:js' as js;
 import 'package:web_components/web_components.dart' show HtmlImport;
 import 'package:polymer/polymer.dart';
 import 'package:polymer_elements/google_apis.dart';
 import 'package:polymer_elements_demos/styles/demo_elements.dart';
+import 'package:polymer_elements/google_client_loader.dart';
 
-/// Silence analyzer [GoogleApis], [DemoElements],
+/// Silence analyzer [DemoElements],
 @PolymerRegister('google-apis-demo')
 class GoogleApisDemo extends PolymerElement {
   GoogleApisDemo.created() : super.created();
 
+  @property List<String> messages = [];
+
   @reflectable
   void loadedShortener(dom.Event event, [_]) {
-    var request = (event.target as dynamic).api['url'].apply('get', [
+    var request = (event.target as dynamic).api['url'].callMethod('get', [
       {'shortUrl': 'http://goo.gl/fbsS'}
     ]);
-    request.execute((resp) {
-      print(resp);
-    });
+    if (event.target is! GoogleClientLoader) {
+      request.execute((resp) {
+        print(resp);
+      });
+    }
   }
 
   @reflectable
   void loaded(dom.Event event, [_]) {
-    var message = '${(event.target as dom.Element).localName} loaded';
-    dom.document.querySelector('#messages').innerHtml += '${message}<br>';
-    print(message);
-    dom.window.console.debug((event.target as dynamic).api);
+    async(() {
+      String message;
+      // TODO(zoechi) why is event target null for google-js-api ?
+      if (event.target == null) {
+        message = '${event.path.last} loaded';
+      } else {
+        message = '${(event.target as dom.Element).localName} loaded';
+      }
+
+      add('messages', message);
+      print(message);
+      if (event.target != null) {
+        dom.window.console.debug((event.target as dynamic).api);
+      }
+    });
   }
 }
